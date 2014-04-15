a35c33f053365d65648d24cdc536ab5527d235c3
brandanbath <brandanbath@gmail.com>
2014-04-14 18:27:25 -0700

    Updated main design view and responsive issues + Working on "Details" view

--- FilmViewer/js/main.js
+++ FilmViewer/js/main.js
@@ -13,9 +13,31 @@ App.controller('CoreCtrl', function($scope, $http){
 	}) 
 	.success(function (data) { 
 		$scope.allMovies = data.movies;
-		$scope.movie = data.movie.year;
-		$scope.movie = data.movie.poster.thumbnail;
-		$scope.movie = data.movie.synopsis;
 	})
 	.error(function (data) { console.log('error', data); });
 })
+
+
+/*		
+Movies.controller('movieController', function($scope, $http) {
+
+    $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json', {
+        params: {
+            apikey: 'kcby9f4hd7xx3pvhfukfwdrd',
+            callback: 'JSON_CALLBACK'
+            page_limit: '16',
+			page: '1',
+			country: 'us'
+        }
+    })
+    .success(function (data) {
+        $scope.movie = data.movie.poster.thumbnail;
+        $scope.cover = data.movie.mpaa_rating;
+        #scope.time = data.movie.runtime;
+        $scope.syn = data.movie.synopsis;
+    })
+    .error(function (data) { console.log('error', data); });
+})
+
+
+*/
