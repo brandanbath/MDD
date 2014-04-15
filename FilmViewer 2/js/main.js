76a58ce29a5c7b15d990d0d71e0d105be541a6fb
brandanbath <brandanbath@gmail.com>
2014-04-15 11:25:41 -0700

    Latest Files

---
+++ FilmViewer 2/js/main.js
@@ -0,0 +1,53 @@
+var App = angular.module('FilmViewer',[]);
+
+App.controller('CoreCtrl', function($scope, $http){
+	$scope.title = 'howdy world';
+	$http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json', { 
+		params: { 
+			apikey: 'kcby9f4hd7xx3pvhfukfwdrd', 
+			callback: 'JSON_CALLBACK',
+			page_limit: '16',
+			page: '1',
+			country: 'us'
+		}
+	}) 
+	.success(function (data) { 
+		$scope.allMovies = data.movies;
+	})
+	.error(function (data) { console.log('error', data); });
+})
+
+
+/*		
+Movies.controller('movieController', function($scope, $http) {
+
+    $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json', {
+        params: {
+            apikey: 'kcby9f4hd7xx3pvhfukfwdrd',
+            callback: 'JSON_CALLBACK'
+            page_limit: '16',
+			page: '1',
+			country: 'us'
+        }
+    })
+    .success(function (data) {
+        $scope.movie = data.movie.poster.thumbnail;
+        $scope.cover = data.movie.mpaa_rating;
+        #scope.time = data.movie.runtime;
+        $scope.syn = data.movie.synopsis;
+    })
+    .error(function (data) { console.log('error', data); });
+})
+*/
+
+
+
+Movies.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+		.when('/Details',{
+			templateUrl:'../views/Details.html',
+			controller: 'movieController'})
+		.otherwise({
+			redirectTo: '/index'
+		});
+}]);
